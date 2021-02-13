32a82e759597c776081547f7a19257dea165e1a0
Anubhav Gupta <anubhavgupta.gupta28@gmail.com>
2021-02-13 12:54:58 +0530

    added new functionalitites

--- src/app/Navbar/Navbar.component.ts
+++ src/app/Navbar/Navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 // import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-Navbar',
@@ -11,10 +12,15 @@ export class NavbarComponent implements OnInit {
   images = [1044, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   contactNumber: string = "+91 98765 43210";
   godrejImagePath = "./ass";
+  safeURL: SafeResourceUrl;
+  currentImgIndex: number = 1;
+  unitPlanImhURL = "./assets/images/unit-plan-" + this.currentImgIndex + ".jpg";
 
   constructor(
-    // private fb: FormBuilder
-  ) { }
+    private _sanitizer: DomSanitizer
+  ) {
+    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/tlLb_rqNnBU");
+  }
 
   // ContactForm = this.fb.group({
   //   Name: ['', [Validators.required]],
@@ -23,6 +29,21 @@ export class NavbarComponent implements OnInit {
 
   // });
   ngOnInit() {
+  }
+  changeUnitPlanImg() {
+    if (this.currentImgIndex == 0) {
+      this.currentImgIndex = 3;
+      // this.unitPlanImhURL = "./assets/images/unit-plan-" + this.currentImgIndex + ".jpg";
+    }
+    else if (this.currentImgIndex == 3) {
+      this.currentImgIndex = 1;
+      // this.unitPlanImhURL = "./assets/images/unit-plan-" + this.currentImgIndex + ".jpg";
+    }
+    else {
+      this.currentImgIndex += 1;
+      // this.unitPlanImhURL = "./assets/images/unit-plan-" + this.currentImgIndex + ".jpg";
+    }
+    this.unitPlanImhURL = "./assets/images/unit-plan-" + this.currentImgIndex + ".jpg";
   }
 
 }
