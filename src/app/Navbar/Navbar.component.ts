dcaa025db60dff027cb51909f5a02f97c7fc5ae6
Anubhav Gupta <anubhavgupta.gupta28@gmail.com>
2021-02-14 16:41:57 +0530

    UI fixes

--- src/app/Navbar/Navbar.component.ts
+++ src/app/Navbar/Navbar.component.ts
@@ -3,6 +3,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DeviceDetectorService, DeviceInfo } from 'ngx-device-detector';
 
 
 @Component({
@@ -22,16 +23,20 @@ export class NavbarComponent implements OnInit {
 
   // images = [1044, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   images = [1, 2, 3].map((n) => `./assets/images/cal/${n}.jpg`);
+  unitPlanImages = [1, 2, 3].map((m) => `./assets/images/unit-plan-${m}.webp`)
   contactNumber: string = "+91 98765 43210";
   godrejImagePath = "./ass";
   safeURL: SafeResourceUrl;
   currentImgIndex: number = 1;
   unitPlanImhURL = "./assets/images/unit-plan-" + this.currentImgIndex + ".jpg";
+  deviceInfo:any;
+  isMobile = false;
 
   constructor(
     private _sanitizer: DomSanitizer,
     private fb: FormBuilder,
     private http: HttpClient,
+    private deviceService : DeviceDetectorService,
     config: NgbModalConfig, private modalService: NgbModal) {
     this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/tlLb_rqNnBU"); // customize default values of modals used by this component tree
     config.backdrop = 'static';
@@ -40,6 +45,8 @@ export class NavbarComponent implements OnInit {
 
 
   ngOnInit() {
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+    this.isMobile = this.deviceService.isMobile();
   }
   open(content: any) {
     this.modalService.open(content);
